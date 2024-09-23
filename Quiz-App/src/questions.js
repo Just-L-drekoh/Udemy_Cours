f1b3b7b17f7fa2e080bc7ce4289d81910060eb00
logan <loloherbo.drekoh@gmail.com>
2024-09-23 15:11:27 +0200

    finish Quiz App Udemy

---
+++ Quiz-App/src/questions.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: "q1",
+    text: "Laquelle des définitions suivantes décrit le mieux React.js ?",
+    answers: [
+      "Une bibliothèque pour construire des interfaces utilisateur avec du code déclaratif.",
+      "Une bibliothèque pour gérer l'état dans les applications web.",
+      "Un framework pour construire des interfaces utilisateur avec du code impératif.",
+      "Une bibliothèque utilisée uniquement pour développer des applications mobiles.",
+    ],
+  },
+  {
+    id: "q2",
+    text: "À quoi servent les hooks de React ?",
+    answers: [
+      "Permettre l'utilisation de l'état et d'autres fonctionnalités de React dans les composants fonctionnels.",
+      "Créer des mises en page réactives dans les applications React.",
+      "Gérer les erreurs au sein de l'application.",
+      "Faire partie de la bibliothèque Redux pour gérer l'état global.",
+    ],
+  },
+  {
+    id: "q3",
+    text: "Pouvez-vous identifier ce qu'est JSX ?",
+    answers: [
+      "Une extension JavaScript qui ajoute une syntaxe semblable à HTML à JavaScript.",
+      "Une bibliothèque JavaScript pour créer des interfaces utilisateur dynamiques.",
+      "Une version spécifique de HTML créée explicitement pour React.",
+      "Un outil pour effectuer des requêtes HTTP dans une application React.",
+    ],
+  },
+  {
+    id: "q4",
+    text: "Quelle est la façon la plus courante de créer un composant dans React ?",
+    answers: [
+      "En définissant une fonction JavaScript qui retourne une valeur à afficher.",
+      "En définissant une balise HTML personnalisée en JavaScript.",
+      "En créant un fichier avec une extension .jsx.",
+      'En utilisant le mot-clé "new" suivi du nom du composant.',
+    ],
+  },
+  {
+    id: "q5",
+    text: 'Que signifie le terme "état React" ?',
+    answers: [
+      "Un objet dans un composant qui contient des valeurs et peut déclencher un rendu du composant lors d'un changement.",
+      "La phase de cycle de vie dans laquelle se trouve un composant React.",
+      "L'état global d'une application React, incluant toutes les props et composants.",
+      "Une bibliothèque pour gérer l'état global dans les applications React.",
+    ],
+  },
+  {
+    id: "q6",
+    text: "Comment affiche-t-on généralement le contenu d'une liste dans les applications React ?",
+    answers: [
+      "En utilisant la méthode map() pour parcourir un tableau de données et retourner du JSX.",
+      "En utilisant la boucle for() pour parcourir un tableau de données et retourner du JSX.",
+      "En utilisant la méthode forEach() pour parcourir un tableau de données et retourner du JSX.",
+      "En utilisant la méthode loop() pour parcourir un tableau de données et retourner du JSX.",
+    ],
+  },
+  {
+    id: "q7",
+    text: "Quelle approche NE peut PAS être utilisée pour afficher du contenu de manière conditionnelle ?",
+    answers: [
+      "En utilisant la syntaxe de template #if.",
+      "En utilisant un opérateur ternaire.",
+      "En utilisant l'opérateur &&.",
+      "En utilisant une déclaration if-else.",
+    ],
+  },
+];
